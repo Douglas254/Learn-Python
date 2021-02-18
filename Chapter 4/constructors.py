b4d9a0ab93ad677b6a3cdff8b1679da75ec6db4b
Douglas254 <douglasobara97@gmail.com>
2021-02-18 21:59:11 +0300

    challenge quiz constructors

--- Chapter 4/constructors.py
+++ Chapter 4/constructors.py
@@ -65,3 +65,47 @@ class Point2:
 
 point = Point2.Zero()
 point.draw()
+
+
+# chalenge 1
+# create a new type person,has name attribute and talk method
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print("I'm talking")
+
+
+person = Person("Douglas254")
+person.talk()
+print(person.name)
+
+
+# Challenge 2
+# create a new animal class, with two methods, legs and eating, and concatenate variables
+
+class Animal:
+    def __init__(self, name_, leg, food):
+        self.name_ = name_
+        self.leg = leg
+        self.food = food
+
+    def name(self):
+        print("")
+
+    def eating(self):
+        print(f"Hello i am {self.name_} and i like eating {self.food}")
+
+    def legs(self):
+        print(f"i have {self.leg} legs")
+
+
+dog = Animal("Dog", 4, "Meat")
+dog.eating()
+dog.legs()
+
+cow = Animal("Cow", 4, "Grass")
+cow.eating()
+cow.legs()
